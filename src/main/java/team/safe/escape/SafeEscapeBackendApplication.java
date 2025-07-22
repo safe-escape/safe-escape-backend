0ca79a2c58cc78ad86ec3c44be175729832fb2f9
soyeon207 <psy010207@naver.com>
2025-07-23 08:28:58 +0900

    🎉 프로젝트 시작

---
+++ src/main/java/team/safe/escape/SafeEscapeBackendApplication.java
@@ -0,0 +1,13 @@
+package team.safe.escape;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeEscapeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SafeEscapeBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/team/safe/escape/SafeEscapeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package team.safe.escape;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafeEscapeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
