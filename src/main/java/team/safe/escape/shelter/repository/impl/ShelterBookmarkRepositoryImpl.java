dd24c621bce827f6fa8b95279065e0304b1de803
soyeon <39696812+soyeon207@users.noreply.github.com>
2025-08-07 19:48:42 +0900

    ✨대피소 찜 취소 API 추가 (#20) (#24)

--- src/main/java/team/safe/escape/shelter/repository/impl/ShelterBookmarkRepositoryImpl.java
+++ src/main/java/team/safe/escape/shelter/repository/impl/ShelterBookmarkRepositoryImpl.java
@@ -3,6 +3,7 @@ package team.safe.escape.shelter.repository.impl;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import team.safe.escape.shelter.entity.ShelterBookmark;
 import team.safe.escape.shelter.repository.ShelterBookmarkRepositoryCustom;
 
 import static team.safe.escape.shelter.entity.QShelterBookmark.shelterBookmark;
@@ -21,5 +22,14 @@ public class ShelterBookmarkRepositoryImpl implements ShelterBookmarkRepositoryC
                 .where(shelterBookmark.id.memberId.eq(memberId)
                         .and(shelterBookmark.id.shelterId.eq(shelterId)))
                 .fetchFirst() != null;
+    }
+
+    @Override
+    public ShelterBookmark findByMemberIdAndShelterId(Long memberId, Long shelterId) {
+        return queryFactory
+                .selectFrom(shelterBookmark)
+                .where(shelterBookmark.id.memberId.eq(memberId)
+                        .and(shelterBookmark.id.shelterId.eq(shelterId)))
+                .fetchFirst();
     }
 }
