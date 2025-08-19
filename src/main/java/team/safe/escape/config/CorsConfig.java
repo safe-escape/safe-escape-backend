48153d80d114483564700d1e2dcc0a1fe84f6995
soyeon207 <psy010207@naver.com>
2025-08-19 17:33:24 +0900

    🔒️ cors 전체 도메인 허용 (#39)

--- src/main/java/team/safe/escape/config/CorsConfig.java
+++ src/main/java/team/safe/escape/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOriginPatterns(List.of("http://localhost:3000"));
+        config.setAllowedOriginPatterns(List.of("*"));
         config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
